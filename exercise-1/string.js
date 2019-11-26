77ca7cc8b356cadf3010eaca5bce9e5db0d70527
Aymeric HERAULT <jorflys@MacBook-Pro-de-Aymeric.local>
2019-11-26 16:51:25 +0100

    camelCase

---
+++ exercise-1/string.js
@@ -0,0 +1,46 @@
+//launch with : docker-compose run node node exercise-1/string.js
+
+    function ucfirst(str) {
+        if(typeof str !== "string" || !str) return '';
+
+        return str[0].toUpperCase() + str.substring(1);
+    }
+
+    console.log(ucfirst("test"));
+    console.log(ucfirst("Test"));
+    console.log(ucfirst(" test"));
+    console.log(ucfirst("test Test tst"));   
+    console.log(ucfirst(""));  
+    console.log(ucfirst(null));  
+    console.log(ucfirst({}));  
+
+    function capitalize(str) {
+        if(typeof str !== "string" || !str) return '';
+
+        return str.toLowerCase().split(" ").map(function(item) {
+            return ucfirst(item);
+        }).join(" ");
+    }
+
+    console.log(capitalize("test"));
+    console.log(capitalize("Test"));
+    console.log(capitalize(" test"));
+    console.log(capitalize("test Test tst"));   
+    console.log(capitalize(""));  
+    console.log(capitalize(null));  
+    console.log(capitalize({}));  
+
+    function camelCase(str) {
+        if(typeof str !== "string" || !str) return '';
+
+        var test = capitalize(str).replace(/ /g,"");
+        return test;
+    }
+
+    console.log(camelCase("test"));
+    console.log(camelCase("Test"));
+    console.log(camelCase(" test"));
+    console.log(camelCase("test Test tst"));   
+    console.log(camelCase(""));  
+    console.log(camelCase(null));  
+    console.log(camelCase({}));  

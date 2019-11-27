2a339e369c5fc6dd2968162a7681b582ae57c5f9
Aymeric <aymeric.herault@gmail.com>
2019-11-27 15:07:00 +0100

    camelCase3

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -9,17 +9,18 @@
     function capitalize(str) {
         if(typeof str !== "string" || !str) return '';
 
-        var test = str.split("_").join(" ");
         return test.toLowerCase().split(" ").map(function(item) {
             return ucfirst(item);
         }).join(" ");
     }
 
     function camelCase(str) {
+        //premiere lettre de chaque mot en MAJ + coller les mots
         if(typeof str !== "string" || !str) return '';
-
-        var test = capitalize(str).replace(/ /g,"");
-        return test;
+    
+        return str.toLowerCase().split(/[^a-zA-Z0-9]/).map(function(item) {
+             return ucfirst(item);
+        }).join("");
     }
 
     console.log(camelCase("test"));
@@ -30,3 +31,30 @@
     console.log(camelCase(null));  
     console.log(camelCase({}));
     console.log(camelCase("ToggleCase is_the coolest")); 
+
+    // function snake_case(str){
+    //     //joint les mots par des underscore
+    // }
+
+    // function leet(str){
+    //     //remplace les voyelle par des chiffres
+    // }
+
+    // function prop_access(prairie, "animal.type.name"){
+    //     //parcours l'objet pour aller chercher le dernier attribut du chemin``
+    //     //si attribut non existant, afficher le chemin jusqu'à l'attribut => "animal.gender not exist"
+    //     //si path vide ou null, renvoyer l'objet complet
+    // }
+
+    // function verlan(str){
+    //     //inverser chaque mot d'une phrase
+    // }
+
+    // function yoda(str){
+    //     //inverser la position des mots d'une phrase
+    // }
+
+    // function vig(str){
+    //     //cryptage => Chiffre de Vigenère
+    //     //wiwipedia + crypto => yzixisfzy
+    // }

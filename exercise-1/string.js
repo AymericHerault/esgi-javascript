86e0c8c1225015f457c18d86995dc5abc2ef7082
Aymeric <aymeric.herault@gmail.com>
2019-11-27 15:57:04 +0100

    leet

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -32,5 +32,23 @@
        }).join("_");       
     }
 
+    function leet(str){
+        //remplace les voyelle par des chiffres
+        // A=4, E=3, I=1, O=0, U=(_), Y=7
+        if(typeof str !== "string" || !str) return ''; 
+        return str.replace(/[aA]/g, "4").replace(/[eE]/g, "3").
+        replace(/[iI]/g, "1").replace(/[oO]/g, "0").replace(/[uU]/g, "_").replace(/[yY]/g, "7");
+        
+        
+    }
+
+    console.log(leet(" test"));
+    console.log(leet("Test"));
+    console.log(leet("AnacondA"));
+    console.log(leet("test Test_tst"));   
+    console.log(leet(""));  
+    console.log(leet(null));  
+    console.log(leet({}));
+    console.log(leet("ToggleCase is_the coolest")); 
 
 
